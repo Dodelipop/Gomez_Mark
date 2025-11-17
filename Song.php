148c237316239a46680e9b6f8aa13c3b5ec396e0
Dodelipop <gomezmark090@gmail.com>
2025-11-17 16:15:38 +0800

    Add files via upload

---
+++ Song.php
@@ -0,0 +1,139 @@
+<?php
+//String
+$title = "On Melancholy Hill";
+$artist = "Gorillaz";
+$mood = "Melancholic";
+$by = $title . ', by' . $artist;
+
+//Int
+$verseCount = 2;
+$repeatChorus = 4;
+
+//Array
+$nouns =["melancholy", "hill", "tree", "day", "dream",
+"sea", "medicine", "submarines", "world",
+"manatee", "love"];
+$verbs =["up", "are", "looking", "get", "want", "can",
+"set", "call", "go", "know", "come", "sits"];
+
+//Song lyrics
+$intro = "(Ah-ah) (Ah-ah)";
+
+$verse1_2 = "Up on Melancholy Hill, there's a plastic tree
+Are you here with me?
+Just looking out on the day of another dream
+Where you can't get what you want, but you can get me
+So let's set out to sea, love
+'Cause you are my medicine when you're close to me
+When you're close to me";
+
+$interlude = "(Ah-ah)";
+
+$verse2_2 = "So call in the submarines, 'round the world we'll go
+Does anybody know, love?
+If we're looking out on the day of another dream?
+(Ah-ah)
+If you can't get what you want, then you come with me
+Up on Melancholy Hill, sits the manatee (Ah-ah-ah-ah), love
+Just looking out for the day when you're close to me
+When you're close to me";
+
+$outro = "Ooh, ooh-ooh-ooh (Ah-ah)
+Ooh-ooh-ooh
+Ooh-ooh-ooh, when you're close to me
+Ooh, ooh-ooh-ooh (Ah-ah)
+Ooh-ooh-ooh
+Ooh-ooh-ooh
+Ooh, ooh-ooh-ooh (Ah-ah)
+Ooh-ooh-ooh";
+
+//Making operators and putting the lyrics into arrays or composing into a song
+$total = $repeatChorus+ $verseCount;
+$lyrics = [$intro, $verse1_2, $interlude, $verse2_2,$outro];
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>body {
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+title{font-size: 18px;}
+
+.head{}
+
+.body {
+padding: 20px;
+width: 700px;
+border: 5px solid #ccc;
+background-color: #008e9b
+box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+float: center;
+margin: 0 auto;
+
+}
+
+.head {
+padding: 20px;
+width: 700px;
+border: 5px solid #ccc;
+background-color:#0089ba
+box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
+float: center;
+margin: 0 auto;
+
+}
+
+.head p {
+text-align: center;
+margin-bottom: 10px;
+
+}
+#Intro {text-align: center;
+    color: rgb(0, 0, 0);
+    text-decoration: solid;}
+h1{
+    text-align: center;
+    color: rgb(0, 0, 0);
+    
+}
+h2 {
+    text-align: center;
+    color: rgb(0, 0, 0);
+}
+p {
+    text-align: center;
+    color: rgb(0, 0, 0);
+    font-size: 18px;
+}
+
+body{
+    background-color:rgb(3, 114, 184);
+    
+}
+
+</style>
+    <div class=head>
+     <title ><?php echo $title?> </title>
+    </head>
+    <body>
+        <h1 href="https://www.youtube.com/watch?v=04mfKJWDSzI"><?php echo $title?> </h1>
+        <h2>On Melancholy Hill Lyrics</h2>
+        </div>
+        <div class=body>
+        <p id="Intro">[Intro]</p>
+        <p><?php echo $lyrics[0]; ?></p>
+        <p>[Verse 1: 2-D]</p>
+        <p><?php echo $lyrics[1]; ?></p>
+        <p>[Interlude]</p>
+        <p><?php echo $lyrics[2]; ?></p>
+        <p>[Verse 2: 2-D]</p>
+        <p><?php echo $lyrics[3]; ?></p>
+        <p>[Outro]</p>
+        <p><?php echo $lyrics[4]; ?></p>
+    </body>
+     </div>
+
+</html>
